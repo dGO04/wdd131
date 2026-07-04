8f4c1b687be3ac2b95da633b084ec6982450aad1
Diego Esquit <diego.esquit.arias@gmail.com>
2026-07-04 07:50:13 -0600

    Home Page assignment - week 01

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+const today = new Date();
+
+document.getElementById('currentyear').innerHTML = today.getFullYear()
+document.getElementById('lastModified').innerHTML = `Last Modification: ${document.lastModified}`
