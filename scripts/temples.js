6e9790de23f132b03a5186792d08febb24c9727b
Diego Esquit <diego.esquit.arias@gmail.com>
2026-07-11 23:31:22 -0600

    Picture album proyect - Week 02

---
+++ scripts/temples.js
@@ -0,0 +1,15 @@
+const currentYear = document.querySelector('#currentYear')
+const lastModification = document.querySelector('#lastModification')
+const hamburgerMenu = document.querySelector('#hamMenu')
+const listItem = document.querySelector('ul')
+
+const today = new Date();
+const year = today.getFullYear();
+
+currentYear.textContent = year;
+lastModification.textContent = `Last Modification: ${document.lastModified}`;
+
+hamburgerMenu.addEventListener('click', () => {
+    listItem.classList.toggle('show')
+    hamburgerMenu.classList.toggle('show')
+});
